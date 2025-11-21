fc3baa2f37e3cf863d7d526fa4e762ddedb77b69
unknowngaming878@gmail.com <unknowngaming878@gmail.com>
2025-11-21 12:34:20 -0400

    Hologram defaults for builder

--- src/main/java/not/savage/airheads/hologram/HologramConfig.java
+++ src/main/java/not/savage/airheads/hologram/HologramConfig.java
@@ -14,42 +14,42 @@ public class HologramConfig {
     public HologramConfig() { /* Configurate No-Args Constructor */ }
 
     // How the Hologram is displayed. FIXED, VERTICAL, HORIZONTAL, CENTERED
-    private String billboardConstraints = BillboardConstraints.VERTICAL.name();
-    private String textAlignment = TextAlignment.CENTERED.name();
+    @Builder.Default private String billboardConstraints = BillboardConstraints.VERTICAL.name();
+    @Builder.Default private String textAlignment = TextAlignment.CENTERED.name();
 
     // The text to display, MiniMessage format.
-    private List<String> hologramText = List.of(
+    @Builder.Default private List<String> hologramText = List.of(
             "<gradient:#a403ff:#d5b7fd><bold>Default Hologram</bold>",
             "<gray>supremeventures.ca/discord"
     );
 
-    private float pitch = 0.0F;
-    private float yaw = 0.0F;
+    @Builder.Default private float pitch = 0.0F;
+    @Builder.Default private float yaw = 0.0F;
 
     // The space between the top of the airhead & the hologram
-    private double hologramOffset = 0.5;
+    @Builder.Default private double hologramOffset = 0.5;
 
     // Used to scale the hologram entity
-    private float scaleX = 1.0F;
-    private float scaleY = 1.0F;
-    private float scaleZ = 1.0F;
+    @Builder.Default private float scaleX = 1.0F;
+    @Builder.Default private float scaleY = 1.0F;
+    @Builder.Default private float scaleZ = 1.0F;
 
     // The rotation of the hologram entity
-    private float translationX = 0.0F;
-    private float translationY = 0.0F;
-    private float translationZ = 0.0F;
+    @Builder.Default private float translationX = 0.0F;
+    @Builder.Default private float translationY = 0.0F;
+    @Builder.Default private float translationZ = 0.0F;
 
-    private int width = 200;
-    private int height = 50;
+    @Builder.Default private int width = 200;
+    @Builder.Default private int height = 50;
 
-    private boolean hasTextShadow = false;
-    private boolean transparentBackground = true;
-    private String backgroundColor = "#40000000";
+    @Builder.Default private boolean hasTextShadow = false;
+    @Builder.Default private boolean transparentBackground = true;
+    @Builder.Default private String backgroundColor = "#40000000";
 
-    private float shadowRadius = 0.0F;
-    private float shadowStrength = 0.0F;
+    @Builder.Default private float shadowRadius = 0.0F;
+    @Builder.Default private float shadowStrength = 0.0F;
 
     // How often the hologram is updated. 1 = every tick, 2 = every other tick, etc. -1 to disable ticking.
-    private int updateIntervalTicks = -1;
-    private int renderDistance = 100;
+    @Builder.Default private int updateIntervalTicks = -1;
+    @Builder.Default private int renderDistance = 100;
 }
